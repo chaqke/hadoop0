e8842291aca631e004fcbc0a1ca7b30911359050
Michael Berkovsky <michael@powerreviews.com>
2010-07-27 15:31:46 -0700

    add

--- src/some/hack/CountReducer.java
+++ src/some/hack/CountReducer.java
@@ -9,20 +9,23 @@ import java.io.IOException;
 
 public class CountReducer extends Reducer <Text,LongWritable,Text,LongWritable>{
 	private LongWritable result = new LongWritable();
-
-	public void reduce(Text key, Iterable<IntWritable> values, Context context)
-			throws IOException, InterruptedException {
-		int sum1 = 0, sum2 = 0;
-		for (IntWritable val : values) {
-			if (val.get() == 1) {
+	@Override
+	public void reduce(Text key, Iterable<LongWritable> values, 
+            Context context
+            ) throws IOException, InterruptedException { 
+		int sum1 = 0, sum2 = 0; 
+		for (LongWritable val : values) {
+			if (val.get() == 1L) {
 				sum1 += 1;
 			} else {
 				sum2 += 1;
-			}
-			long sum = (sum1 << 16) | sum2;
-			result.set(sum);
-			context.write(key, result);
+			} 
 		}
+		//long sum = (sum1 << 16) | sum2;
+		result.set(1L);
+		String s = sum1 + "_" + sum2 + "_" +  key.toString(); 
+		key.set(s );
+		context.write(key, result);
 	}
  
 }
--- src/some/hack/Executor.java
+++ src/some/hack/Executor.java
@@ -2,6 +2,7 @@ package some.hack;
 
 import org.apache.hadoop.conf.Configuration; 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -12,17 +13,19 @@ public class Executor {
 	public static void main(String [] args) throws Exception{
 		Configuration conf = new Configuration();
 	    String inPath = "/temp/in";
-	    String outPath = "/temp/out";//TODO
-	     
-	    Job job = new Job(conf, "word count");
-	    job.setJarByClass(WordCount.class);
+	    String outPath = "/temp/out"; 
+	    
+	    Job job = new Job(conf, "word-count");
+	    job.setNumReduceTasks(1);
+	    job.setJarByClass(Executor.class);
 	    job.setMapperClass(CountMapper.class);
 	    job.setCombinerClass(CountReducer.class);//TODO
 	    job.setReducerClass(CountReducer.class);
 	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(IntWritable.class);
+	    job.setOutputValueClass(LongWritable.class);
 	    FileInputFormat.addInputPath(job, new Path(inPath));
 	    FileOutputFormat.setOutputPath(job, new Path(outPath));
+	    
 	    System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }

00d27e7d2c6e38ec81be599e95cfe006210ad533
Michael Berkovsky <michael@powerreviews.com>
2010-07-27 12:36:47 -0700

    stuff

---
+++ src/some/hack/CountMapper.java
@@ -0,0 +1,9 @@
+package some.hack;
+
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.io.LongWritable;
+ 
+
+public class CountMapper extends CountMapper<Object, Text, Text, IntWritable>{
+
+}
